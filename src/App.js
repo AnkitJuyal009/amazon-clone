ecb8f0a0375432105837cc17ff3c47fa80205ad5
AnkitJuyal009 <ankitjuyal2000@gmail.com>
2020-09-13 17:56:21 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Checkout from "./components/Checkout";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <Header />
+        <Switch>
+          <Route path="/checkout">
+            <Checkout />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/firebase/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBDob5kQ64YdKO0-KR7YOXq8rJUpDLrHPE",
+  authDomain: "clone-32466.firebaseapp.com",
+  databaseURL: "https://clone-32466.firebaseio.com",
+  projectId: "clone-32466",
+  storageBucket: "clone-32466.appspot.com",
+  messagingSenderId: "649630124615",
+  appId: "1:649630124615:web:c02707da4cbebaf09fbf62",
+  measurementId: "G-S6KH850WW0",
+};
